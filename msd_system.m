03f1a8f7254e17ce29fdfef5b6fe1da8ee8351f4
rituushah <ritu-shah@hotmail.com>
2016-07-21 21:40:23 -0400

    Update msd_system.m

--- msd_system.m
+++ msd_system.m
@@ -6,7 +6,7 @@ global J5 m10 K8 radius f3 R1 C2 e1 e2
 %we have x'=Ax; A is the dynamic matrix of our system that we have from
 %state equations. t is time. 
 
-xprime=[ 1/(R1*C2)    0        0            0;
+xprime=[ -1/(R1*C2)    0        0            0;
          0          -1/J5      -radius/K8      0 ;
         0          radius/J5       0           -1/m10 ;
-        0              0            1/K8           0   ] *    [x(1) ; x(2) ; x(3); x(4)] + [f3; e1; 0; e2]; 
+        0              0            1/K8           0   ] *    [x(1) ; x(2) ; x(3); x(4)] + [-f3; e1; 0; e2]; 
